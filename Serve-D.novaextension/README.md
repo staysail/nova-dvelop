69ae2776280a0b27630e0d09090f928db9d17fc8
Garrett D'Amore <garrett@damore.org>
2022-11-05 00:46:43 +0000

    Add support for automatic formatting.

--- Serve-D.novaextension/README.md
+++ Serve-D.novaextension/README.md
@@ -5,7 +5,10 @@
 **Serve-D** provides deep integration with [**D**][1] via the [Serve-D][2] Language Server.
 
 This includes support for syntax highlighting via a [Tree-sitter][3] D [grammar][4], indentation,
-and folding.  If you have installed our _D_ extension, you may disable that as this
+folding, and automatic formatting via `dfmt`, including optional formatting of your
+code on save.
+
+If you have installed our _D_ extension, you may disable that as this
 extension includes all of that functionality as well.
 
 ![](https://raw.githubusercontent.com/staysail/nova-serve-d/main/screenshot.png)
@@ -15,17 +18,21 @@ extension includes all of that functionality as well.
 Serve-D requires the actual `serve-d` language server to be installed on your mac.
 Most often this will be located in `/usr/local/bin`, which is the default.
 
-
-As of this release, it is required to update to a current nightly build,
+As of this release, it is required to update to a recent nightly or pre-release build,
 or to build the server yourself.  In particular, releases **0.7.4** and earlier
-will *not* work with this extension.  (Hopefully a new version will be
-released soon, and we can update this message here.)
+will *not* work with this extension.
+
+Version **0.8.0** is in beta and it should work fine.
 
 ## Usage
 
 Serve-D runs any time you open a local project, automatically lints all open
 files, then reports errors and warnings in Nova's **Issues** sidebar
 and the editor gutter.
+
+You can use the `Format D File` from the editor menu, and you can also enable
+this to happen automatically when saving on a per workspace basis by setting
+it in the project settings.
 
 ### Configuration
 
@@ -35,9 +42,6 @@ Serve-D preferences.  The `Language Server Path` is the full
 path to the server.
 
 ## Future Work
-
-Automatic formatting, including additional configuration using either
-the built-in `dfmt` or the `sdfmt` utility is expected.
 
 There are settings for different types of formatting options that
 we plan to add, so projects can adjust their formatting to taste.
--- Serve-D.novaextension/Scripts/main.js
+++ Serve-D.novaextension/Scripts/main.js
@@ -3,7 +3,82 @@ var langserver = null;
 exports.activate = function() {
 	// Do work when the extension is activated
 	langserver = new DLanguageServer();
+
+	nova.workspace.onDidAddTextEditor(editor => {
+		if (editor.document.syntax != 'd') return;
+		editor.onWillSave(editor => {
+			const formatOnSave = nova.workspace.config.get('d.dfmt.formatOnSave');
+			if (formatOnSave) {
+				return formatFile(editor);
+			}
+		});
+	});
+	nova.commands.register('dfmt.format', formatFileCmd);
 };
+
+async function formatFileCmd(editor) {
+	try {
+		await formatFile(editor);
+	} catch (err) {
+		nova.workspace.showErrorMessage(err);
+	}
+}
+
+// Nova ranges are absolute character offsets
+// LSP ranges based on line/column.
+function lspRangeToNovaRange(document, range) {
+	let pos = 0;
+	let start = 0;
+	let end = document.length;
+	const lines = document.getTextInRange(new Range(0, document.length)).split(document.eol);
+	for (let line = 0; line < lines.length; line++) {
+		if (range.start.line == line) {
+			start = pos + range.start.character;
+		}
+		if (range.end.line == line) {
+			end = pos + range.end.character;
+			break; // we finished, so no need to keep scanning the doc
+		}
+		pos += lines[line].length + document.eol.length;
+	}
+	let res = new Range(start, end);
+	return res;
+}
+
+function lspApplyEdits(editor, edits) {
+	return editor.edit(textEditorEdit => {
+		for (const change of edits.reverse()) {
+			const range = lspRangeToNovaRange(editor.document, change.range);
+			textEditorEdit.replace(range, change.newText);
+		}
+	});
+}
+
+async function formatFile(editor) {
+	if (langserver && langserver.languageClient) {
+		var cmdArgs = {
+			textDocument: {
+				uri: editor.document.uri,
+			},
+			options: {
+				tabSize: editor.tabLength,
+				insertSpaces: editor.softTabs,
+			},
+			// TBD: options
+		};
+		var client = langserver.languageClient;
+		if (!client) {
+			console.error('no language client');
+			return;
+		}
+		const changes = await client.sendRequest('textDocument/formatting', cmdArgs);
+
+		if (!changes) {
+			return;
+		}
+		await lspApplyEdits(editor, changes);
+	}
+}
 
 exports.deactivate = function() {
 	// Clean up state before the extension is deactivated
